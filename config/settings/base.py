1da57320fecb47e8923907bd849730822707eba8
ssuntol916 <ssuntol916@gmail.com>
2024-09-21 13:34:17 +0700

    celery 설정 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -204,3 +204,11 @@ LOGGING = {
         },
     }
 }
+
+# Celery Settings
+CELERY_TIMEZONE = 'Asia/Ho_Chi_Minh'
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
