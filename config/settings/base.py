9a374d230b56fd7057b857b8eee0f39f289c46cb
ssuntol916 <ssuntol916@gmail.com>
2024-09-21 10:23:31 +0700

    celery 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Celery
+    'celery',
+    'celery_progress',
 ]
 
 MIDDLEWARE = [
