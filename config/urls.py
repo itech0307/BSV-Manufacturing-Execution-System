2ec2ff86415c04477ded0e8c18c2b03510e2102b
ssuntol916 <ssuntol916@gmail.com>
2024-09-21 13:54:01 +0700

    main 페이지 redirect 설정

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_main(request):
+    return redirect('common:main')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('common/', include('common.urls')),
+    path('', redirect_to_main, name='home'),
 ]
