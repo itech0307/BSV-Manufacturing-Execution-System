8af22ecb20f6979bfd7f3b198660b4bf948729ba
ssuntol916 <ssuntol916@gmail.com>
2024-09-21 15:04:09 +0700

    인벤토리 모델 생성

--- inventory_management/models.py
+++ inventory_management/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.category_name
+
+class Supplier(models.Model):
+    supplier_name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.supplier_name
+
+class RawMaterial(models.Model):
+    material_name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.supplier}-{self.material_name}'
