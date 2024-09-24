3b291f1e2e588d66ff101a88ffb89efa76e58464
ssuntol916 <ssuntol916@gmail.com>
2024-09-24 13:43:47 +0700

    ProductionLot 모델 생성

--- data_monitoring/models.py
+++ data_monitoring/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from production_management.models import ProductionPlan
+from itertools import chain
+from datetime import datetime, timedelta
 
 class DryMix(models.Model):
     production_plan = models.ForeignKey(ProductionPlan, on_delete=models.CASCADE)
@@ -70,3 +72,43 @@ class Inspection(models.Model):
 
     def __str__(self):
         return f"{self.production_plan.sales_order.order_no}-{self.production_plan.plan_date}"
+
+class ProductionLot(models.Model):
+    lot_number = models.CharField(max_length=20, unique=True)
+    create_date = models.DateTimeField(default=timezone.now)
+
+    @classmethod
+    def generate_roll_lot(cls):
+        today = datetime.now().strftime('%m%d')
+        three_days_ago = datetime.now() - timedelta(days=3)
+        
+        # 'DryLine'와 'RP'의 phase 검색
+        production_phases = list(chain(
+                DryLine.objects.filter(),
+                Delamination.objects.filter()
+            ))
+        
+        roll_lots = set()
+        
+        for phase in production_phases:
+            if isinstance(phase, DryLine):
+                roll_lots.add(phase.pd_lot)
+            elif isinstance(phase, Delamination):
+                roll_lots.add(phase.dlami_lot)
+        
+        existing_counts = sorted(
+            int(roll_lot.split('-')[-1][:-1]) if roll_lot.split('-')[-1][-1] in 'AB'
+            else int(roll_lot.split('-')[-1])
+            for roll_lot in roll_lots 
+            if '-' in roll_lot
+        )
+        
+        count = 1
+        for existing_count in existing_counts:
+            if existing_count != count:
+                break
+            count += 1
+        
+        new_lot_number = f"{today}-{count}"
+        cls.objects.create(lot_number=new_lot_number)
+        return new_lot_number
