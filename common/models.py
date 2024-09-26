7ca3e5d1a62f077c2a328da140404b6fe1ffab6d
ssuntol916 <ssuntol916@gmail.com>
2024-09-26 10:02:21 +0700

    Profile 모델에 직책 추가

--- common/models.py
+++ common/models.py
@@ -6,6 +6,17 @@ import uuid
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    POSITION_CHOICES = [
+        ('PD', 'Production Director'),
+        ('PM', 'Production Manager'),
+        ('PS', 'Production Staff'),
+        ('SD', 'Sales Director'),
+        ('SM', 'Sales Manager'),
+        ('SS', 'Sales Staff'),
+    ]
+    
+    position = models.CharField(max_length=3, choices=POSITION_CHOICES, blank=True)
     email_confirmed = models.BooleanField(default=False)
     activation_token = models.UUIDField(default=uuid.uuid4, editable=False)
 
