df03cbf75c34568ceafa2e0083efd1095155dce4
ssuntol916 <ssuntol916@gmail.com>
2024-09-29 16:02:34 +0700

    개발 모델 생성

--- production_management/models.py
+++ production_management/models.py
@@ -70,3 +70,54 @@ class ProductionPlan(models.Model):
 
     def __str__(self):
         return f"{self.sales_order.order_no}-{self.plan_date}"
+
+class Development(models.Model):
+    developer = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    purpose = models.CharField(max_length=200)
+    category = models.CharField(max_length=50)
+    deadline = models.DateField(null=True)
+    content = models.TextField()
+    status = models.CharField(
+        max_length=10,
+        choices=[('Progress', 'Progress'), ('Complete', 'Complete')],
+        default='Progress'
+    )
+    create_date = models.DateTimeField(default=timezone.now)
+    modify_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class DevelopmentOrder(models.Model):
+    development = models.ForeignKey(Development, on_delete=models.CASCADE)
+    order_no = models.CharField(max_length=14, unique=True, editable=False)
+    order_information = models.JSONField()
+    status = models.BooleanField(null=True)  # 등록 시 null, 완료 시 True, 삭제 시 false
+    create_date = models.DateTimeField(default=timezone.now)
+    modify_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.order_no:
+            # 현재 날짜를 'YYMMDD' 형식으로 가져옵니다.
+            date_str = self.create_date.strftime('%y%m%d')
+            prefix = 'DEV0'
+            # 해당 날짜에 생성된 마지막 오더를 찾아서 순서 번호를 증가시킵니다.
+            last_order = DevelopmentOrder.objects.filter(order_no__startswith=f'{prefix}{date_str}').order_by('order_no').last()
+            if last_order:
+                seq_no = int(last_order.order_no.split('-')[-1]) + 1
+            else:
+                seq_no = 1
+            # 새 주문 번호를 생성합니다.
+            self.order_no = f'{prefix}{date_str}-{seq_no}'
+        super(DevelopmentOrder, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_no
+
+class DevelopmentComment(models.Model):
+    development = models.ForeignKey(Development, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    modify_date = models.DateTimeField(auto_now=True)
