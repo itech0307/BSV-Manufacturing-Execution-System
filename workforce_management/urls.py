888c7670a52bb85f1dcee30a29c993c479072679
ssuntol916 <ssuntol916@gmail.com>
2024-10-13 13:35:36 +0700

    url 수정

--- workforce_management/urls.py
+++ workforce_management/urls.py
@@ -6,7 +6,7 @@ app_name = 'workforce_management'
 urlpatterns = [
     path('worker/list/', views.worker_list, name='worker_list'),
     path('worker/register/', views.worker_register, name='worker_register'),
-    path('worker/<str:worker_code>/', views.worker_detail, name='worker_detail'),
-    path('worker/modify/<str:worker_code>/', views.worker_modify, name='worker_modify'),
-    path('worker/comment/create/<str:worker_code>/', views.worker_comment_create, name='worker_comment_create'),
+    path('worker/<int:worker_id>/', views.worker_detail, name='worker_detail'),
+    path('worker/modify/<int:worker_id>/', views.worker_modify, name='worker_modify'),
+    path('worker/comment/create/<int:worker_id>/', views.worker_comment_create, name='worker_comment_create'),
 ]
